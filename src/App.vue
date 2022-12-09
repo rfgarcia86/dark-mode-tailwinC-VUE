7b6722bee5a10fce7bccfd7e79818932460a6b22
¨rfgarcia86¨ <ricardo.86.fernandez@gmail.com>
2022-12-08 21:11:10 -0500

    dia 1 dark mode taildwinCSS

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark');    
+} else {
+  document.documentElement.classList.remove('dark')  
+}
+
+// // Whenever the user explicitly chooses light mode
+// localStorage.theme = 'light'
+
+// // Whenever the user explicitly chooses dark mode
+// localStorage.theme = 'dark'
+
+// // Whenever the user explicitly chooses to respect the OS preference
+// localStorage.removeItem('theme')
+
+const toggleDark = ref(document.documentElement.className === 'dark');
+
+const changeDarkMode = () =>{
+    toggleDark.value = document.documentElement.classList.toggle('dark');
+    toggleDark.value ? (localStorage.theme = 'dark') : (localStorage.theme = 'light')
+  }
+</script>
+
+<template>
+  <main class="container mx-auto text-center mt-5">
+    <h1 class="text-4xl dark:bg-gray-800 dark:text-white">VUE APP</h1>
+    <button class="bg-gray-800 text-white rounded-xl py-3 px-4 mt-5" @click="changeDarkMode">Ligth/Dark</button>
+  </main>
+</template> 
